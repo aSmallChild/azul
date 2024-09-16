0aa436ec792f3b513c0a2b0a440852a0b3517364
Simeon <totally@legit.nz>
2024-09-16 20:11:40 +1200

    fix colour ids

--- ui-vue/src/util/colours.js
+++ ui-vue/src/util/colours.js
@@ -1,9 +1,9 @@
 export const hexColours = [
     { id: 0, hex: '#05c', name: 'blue' },
     { id: 1, hex: '#d90', name: 'yellow' },
-    { id: 3, hex: '#f00', name: 'red' },
-    { id: 2, hex: '#222', name: 'black' },
-    { id: 1, hex: '#3de', name: 'light blue' },
+    { id: 2, hex: '#f00', name: 'red' },
+    { id: 3, hex: '#222', name: 'black' },
+    { id: 4, hex: '#3de', name: 'light blue' },
     { id: 5, hex: '#9d1', name: 'light green' },
     { id: 6, hex: '#160', name: 'green' },
 ];
